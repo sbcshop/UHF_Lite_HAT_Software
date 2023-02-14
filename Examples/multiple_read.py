13a7643575fce7fe41048373ffa404d4a681e910
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-02-14 11:39:18 +0530

    Update multiple_read.py

--- Examples/multiple_read.py
+++ Examples/multiple_read.py
@@ -1,7 +1,7 @@
 import time
 import sys
-sys.path.insert(0,'/home/pi/Desktop/UHF_LITE_HAT/Library')# Make sure use your library path here
-from uhf import UHF
+sys.path.insert(0,'/home/pi/UHF_Lite_HAT_Software-main/Library')# Make sure use your library path here
+from uhf import UHF 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
