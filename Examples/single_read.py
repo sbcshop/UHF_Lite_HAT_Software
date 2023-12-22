55a6a685b059420e9139391f1ec3c110cdec80ef
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-12-22 11:35:54 +0530

    Update single_read.py

--- Examples/single_read.py
+++ Examples/single_read.py
@@ -16,6 +16,14 @@ baudrate ='115200'
 port     ='/dev/ttyS0'
 
 uhf = uhf.UHF(port,baudrate)
+
+'''
+Uncomment corresponding section to increase reading range,
+you will have to set the region as per requirment
+'''
+#uhf.setRegion_EU() 
+#uhf.setRegion_US()
+
 rev = uhf.single_read()
 if rev is not None:
    print('EPC = ',rev[8:20])
