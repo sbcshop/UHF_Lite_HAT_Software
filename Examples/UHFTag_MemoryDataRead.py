fc6bec03b320b55b8b328f8e9b0bf4d5c8c4f09e
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-12-22 11:31:37 +0530

    Update UHFTag_MemoryDataRead.py

--- Examples/UHFTag_MemoryDataRead.py
+++ Examples/UHFTag_MemoryDataRead.py
@@ -3,16 +3,30 @@ Code to perform read operation from Memory of UHF Tags,
 Reserved, EPC, TID and User are different memory options available
 
 '''
-from machine import UART, Pin,SPI
-import time,utime
-from uhf import UHF #include uhf library file
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+import serial
+import sys
+sys.path.append("..")
+from Library import uhf 
 
-#UHF enable pin connected at GP4 
-enable_pin = machine.Pin(4, machine.Pin.OUT) # set pin as OUTPUT
-enable_pin.value(0) # LOW value enables UHF module, HIGH to disable module
+from uhf import UHF
+from os import path
+import time as sleep
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-baudrate = 115200   # communication baudrate
-uhf = UHF(baudrate) # create instance for class UHF
+GPIO.setup(7,GPIO.OUT)
+
+GPIO.output(7,GPIO.LOW) # LOW to enable the UHF, HIGH to disable UHF
+
+
+baudrate ='115200' # default baudrate
+port     ='/dev/ttyS0'
+
+
+uhf = uhf.UHF(port,baudrate)
 
 '''
 Memory Bank 
